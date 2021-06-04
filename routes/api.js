51222d45e535d246d6c6b2b34362b3a89620d46b
salomohutapea <hutapeasalomo@gmail.com>
2021-06-04 22:29:00 +0700

    API Fix

--- routes/api.js
+++ routes/api.js
@@ -29,22 +29,20 @@ async function addReport() {
 function paddingArray(sequence, padding) {
     const insertPadding = padding - sequence.length;
     for (let i = 0; i < insertPadding; i++) {
-        sequence.push(0);
+        sequence.unshift(0);
     }
     return sequence;
 }
 // function for processing Machine Learning Model
 function textToSequence(rawInput) {
     const input = Array.isArray(rawInput) ? rawInput : [rawInput]
-    console.log(input)
+    console.log("\n\nINPUT\n" + input)
     return input.reduce(function (finalResult, currentInput) {
-        const words = currentInput.split(" ")
-        console.log(words)
+        const words = currentInput.toLowerCase().split(" ")
         const sequence = words.reduce(function (result, current) {
             if (vocab[current]) result.push(vocab[current])
             return result
         }, [])
-        console.log(sequence)
         finalResult.push(paddingArray(sequence, 53))
         return finalResult
     }, [])
@@ -52,9 +50,14 @@ function textToSequence(rawInput) {
 
 function processOutput(output) {
     return output.reduce(function (result, current) {
-        result.push(current[0]);
+        result.push(current[1]);
+        // console.log(current)
         return result;
     }, []);
+}
+
+function argMax(array) {
+    return [].map.call(array, (x, i) => [x, i]).reduce((r, a) => (a[0] > r[0] ? a : r))[1];
 }
 
 router.get('/', async function (req, res, next) {
@@ -62,16 +65,23 @@ router.get('/', async function (req, res, next) {
         const modelUrl = "https://storage.googleapis.com/novia_model/models/model.json"
         if (!model) model = await tf.loadLayersModel(modelUrl)
         const input = textToSequence(req.query.input)
-        const result = model.predict(tf.tensor2(input))
-        if (tf.argMax(result) == 0) {
-            let importance = "Low";
-            console.log(importance);
-          } 
-        else if (tf.argMax(result) == 1){
-            let importance = "High";
-            console.log(importance);
-          }
-        return res.json(processOutput(await result.array()))
+        const result = await model.predict(tf.tensor2d(await input))
+        const resultImportance = argMax(result.dataSync())
+
+        console.log("\nRESULT")
+        console.log(resultImportance + "\n")
+
+        if (resultImportance === 0) {
+            return res.json({
+                report: req.query.input,
+                importance: "low"
+            })
+        } else if (resultImportance === 1) {
+            return res.json({
+                report: req.query.input,
+                importance: "high"
+            })
+        }
     } catch (e) {
         console.log(e);
         return res.send('Model Error')
